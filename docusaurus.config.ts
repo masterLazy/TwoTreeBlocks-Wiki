01c28f69f0497a9cde5078f680162acca4103098
mLazy <95967139+masterLazy@users.noreply.github.com>
2024-09-07 09:42:04 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Repair-Technology/docusaurus-v3/tree/main/',
+            'https://github.com/masterLazy/TwoThreeBlocks-Wiki/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Repair-Technology/docusaurus-v3/tree/main/',
+            'https://github.com/masterLazy/TwoThreeBlocks-Wiki/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
