b60f4844cb6a2df69ce486a1df9006404fba5e53
mLazy <95967139+masterLazy@users.noreply.github.com>
2024-09-15 23:09:29 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home(): JSX.Element {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <center><img src="img/ttb.svg" style={{width:'20em'}}/></center>
+        <center><img src="img/ttb.jpg" style={{width:'20em'}}/></center>
       </main>
     </Layout>
   );
