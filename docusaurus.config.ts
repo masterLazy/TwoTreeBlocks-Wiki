d776a48e48ddd2b962b7adc74c17282417b4147e
mLazy <95967139+masterLazy@users.noreply.github.com>
2024-09-27 19:26:11 +0800

    Fix docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://masterLazy.github.io/TwoThreeBlocks-Wiki',
+  url: 'https://masterLazy.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/TwoThreeBlocks-Wiki/',
