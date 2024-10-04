2f64ec79ccaa32b343c2cbf5adc7b206eea98321
mLazy <95967139+masterLazy@users.noreply.github.com>
2024-10-05 00:38:53 +0800

    Fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home(): JSX.Element {
                             left: 0,
                             width: '100%',
                             height: '100%',
-                            object-fit: cover
+                            'object-fit': 'cover'
                         }}>
                     </div>
                 </center>
