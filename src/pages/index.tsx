53bf2c8f016baf7c250ffe51e6c2908febfc3c61
mLazy <95967139+masterLazy@users.noreply.github.com>
2024-10-05 00:14:02 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,35 +7,51 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            了解详情
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <Heading as="h1" className="hero__title">
+                    {siteConfig.title}
+                </Heading>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/intro">
+                        了解详情
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-              <center><img src="https://i.ibb.co/SrQrTjG/ttb.jpg" style={{display: 'flex'}}/></center>
-      </main>
-    </Layout>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <Layout
+            description="Description will go into a meta tag in <head />">
+            <HomepageHeader />
+            <main>
+                <center>
+                    <div style={{
+                        height: 20em;
+                        width: auto;
+                        overflow: hidden;
+                        position: relative;
+                    }}>
+                        <img src="https://i.ibb.co/s3n4x7n/ttb.jpg" style={{
+                            position: absolute;
+                            top: 0;
+                            left: 0;
+                            width: 100 %;
+                            height: 100 %;
+                            object- fit: cover;
+                        }}>
+                    </div>
+                </center>
+            </main>
+        </Layout>
+    );
 }
